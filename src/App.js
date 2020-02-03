53af8f7847f0744b4f3802c0bee6db5257a9f342
Dena Cho <denaexplore@gmail.com>
2020-02-03 23:40:07 +0100

    mins 40

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ const API_KEY = "4b7b027ebde2845e062e187c8999dd65";
 class App extends React.Component {
   getWeather = async e => {
     e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${API_KEY}&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=&{city},&{country}uk&appid=${API_KEY}&units=metric`
     );
     const data = await api_call.json();
     console.log(data);
