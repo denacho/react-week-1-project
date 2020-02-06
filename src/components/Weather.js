c0709eefd2f24ebdaff764b301b0ddd5894f5fe0
Dena Cho <denaexplore@gmail.com>
2020-02-06 23:21:02 +0100

    mins 60

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-class Weather extends React.Component {
-  render() {
-    return <div>Weather Component</div>;
-  }
-}
-
+const Weather = props => (
+  <div>
+    {props.city && props.country && (
+      <p>
+        Location: {props.temperature}, {props.city}
+      </p>
+    )}
+    {props.temperature && <p>Temperature:{props.country}</p>}
+    {props.humidity && <p>Humidity:{props.humidity}</p>}
+    {props.description && <p>Condition:{props.description}</p>}
+    {props.error && <p> {props.error}</p>}
+  </div>
+);
 export default Weather;
